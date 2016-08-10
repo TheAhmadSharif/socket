606e9bc8f2aa903f74aec2623edae6b0783dc8b3
Ahmad <ahmadshorif@gmail.com>
2016-08-10 21:36:58 +0600

    socket

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 app.get('/', function (request, response) {
 	response.sendfile('index.html');	
 });
 
+
+
+io.on('connection', function(mofiz){
+		console.log('A user connected');
+
+			//Send a message after a timeout of 4seconds
+	  setTimeout(function(){
+	    mofiz.send('Sent a message 1 seconds after connection!');
+	  }, 1000);
+
+		mofiz.on('disconnect', function () {
+			console.log('Hello Disconnectd');
+		});
+});
+
 http.listen(3000, function() {
 	console.log('Listen on :3000');
 });
+
+
